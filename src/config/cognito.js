931d824e0d65703a41871b51c1d1c03ee664bbff
prathyushit <131624513+prathyushit@users.noreply.github.com>
2024-12-23 23:47:07 -0500

    Update cognito.js

--- src/config/cognito.js
+++ src/config/cognito.js
@@ -1,9 +1,6 @@
 // prettier-ignore
 
 export const poolData = {
-  // UserPoolId: "us-east-1_iX5Nr1Yxx", contractor acc
-  // ClientId: "1d49a8vrt0c7mcba0jm050sgeq"
-  UserPoolId: "us-east-1_YOVjm5Lej", 
-  ClientId: "5c5eesiubskvd0eu0rnrnch78i"
-
+  UserPoolId: process.env.REACT_APP_USER_POOL_ID,
+  ClientId: process.env.REACT_APP_CLIENT_ID,
 }
